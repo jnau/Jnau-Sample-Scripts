7ec2aec8c58b3b46e5b6772e8546df1c886689ad
Jessica Au <jnau@ucdavis.edu>
2018-09-22 00:08:34 -0700

    Add files via upload

---
+++ Matlab-Octave/Numerical Analysis/FiniteElement.m
@@ -0,0 +1,82 @@
+
+
+
+function C = finelement(N, x, p,q,f)
+
+    h = zeros(N,1);
+    
+    Q1 = zeros(N,1);
+    Q2 = zeros(N,1);
+    Q3 = zeros(N,1);
+    Q4 = zeros(N,1);
+    Q5 = zeros(N,1);
+    Q6 = zeros(N,1);
+    a = zeros(N,1);
+    b = zeros(N,1);
+    alpha = zeros(N,1);
+    beta = zeros(N,1);
+    chi = zeros(N,1);
+    z = zeros(N,1);
+    c = zeros(N,1);
+    
+    for i = 1:N
+        
+        h(i) = x(i+1)-x(i);
+        
+        if i <= N-1
+
+            Q1(i) = (h(i)/12)*(q(x(i))+q(x(i+1)));
+            Q3(i) = (h(i)/12)*(3*q(x(i))+q(x(i+1)));
+            Q6(i) = (h(i)/6)*(2*f(x(i))+f(x(i+1)));
+            
+        end
+        
+        if i > 1
+
+            Q2(i-1) = (h(i)/12)*(3*q(x(i))+q(x(i-1)));
+            Q5(i-1) = (h(i-1)/6)*(2*f(x(i))+f(x(i-1)));
+            Q4(i-1) = (p(x(i))+p(i-1))*(1/(2*h(i-1)));
+            
+        end
+  
+    end
+    
+    for k = 1:N-1
+        
+        alpha(k) = Q4(k) + Q4(k+1) + Q2(k) + Q3(k);
+        beta(k) = -Q4(k+1) + Q1(k);
+        b(k) = Q5(i) + Q6(i); 
+
+    end
+    
+    alpha(N) = Q4(N) + Q4(N+1) +Q2(N) + Q3(N);
+    b(N) = Q5(N) + Q6(N);
+    
+    a(1) = alpha(1);
+    chi(1) = beta(1)/alpha(1);
+    z(1) = b(1)/a(1);
+    
+    for j = 2:N-1
+        
+        a(j) = alpha(j) - beta(j-1)*chi(j-1);
+        chi(j) = beta(j)/a(j);
+        z(j) = (b(j) - beta(j-1)*z(j-1))/a(j);
+        
+    end
+
+    a(N) = alpha(N) - beta(N-1)*chi(N-1);
+    z(N) = (b(N) - beta(N-1)*z(N-1))/a(N);
+    
+    
+    c(N) = z(N); 
+    
+    for l = N-1:1
+        
+        c(l) = z(l)-chi(l)*c(l+1);
+        
+    end
+    
+    C = c;
+
+
+end
---
+++ Matlab-Octave/Numerical Analysis/RungeKutta.m
@@ -0,0 +1,42 @@
+function R = rungekutta(x0,y0,a,b, h)
+
+
+    %define functions
+    fx = @(t,x,y) -1000*x + y;
+    fy = @(t,x,y) -1/10*y;
+
+    N = (b-a)/h;
+    y = ones(N+1,1);
+    x = ones(N+1,1);
+    t = ones(N+1,1);
+    
+    y(1) = y0;
+    x(1)= x0;
+    t(1) = a;
+    
+    for i = 1:N
+        q1x = fx(t(i), x(i), y(i));
+        q1y = fy(t(i), x(i), y(i));
+        
+        q2x = fx(t(i)+(h/2), x(i)+((h/2)*q1x), y(i)+((h/2)*q1y));
+        q2y = fx(t(i)+(h/2), x(i)+((h/2)*q1x), y(i)+((h/2)*q1y));
+        
+        q3x = fx(t(i)+(h/2), x(i)+((h/2)*q2x), y(i)+((h/2)*q2y));
+        q3y = fx(t(i)+(h/2), x(i)+((h/2)*q2x), y(i)+((h/2)*q2y));
+        
+        q4x = fx(t(i)+(h), x(i)+(h*q3x), y(i)+(h*q3y));
+        q4y = fx(t(i)+(h), x(i)+(h*q3x), y(i)+(h*q3y));
+        
+        x(i+1) = (h/6)*(q1x + 2*q2x + 2*q3x + q4x);
+        y(i+1) = (h/6)*(q1y + 2*q2y + 2*q3y + q4y);
+        
+        t(i+1) = t(i) + h;
+        
+    end
+    
+figure
+plot(t,y);
+R = y;
+
+
+end
